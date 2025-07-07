a228bce6bce4a7b04dae6bdc3a14900d6fca9ad4
olonok69 <olonok@gmail.com>
2025-07-07 06:09:43 +0100

    build llm

--- servers/server2/prompts/promptsRegistry.js
+++
@@ -1,7 +0,0 @@
-// Import prompt registration function
-import { registerPrompt } from './index.js';
-
-// For now, we'll start without any prompts registered
-// You can add prompts here as needed
-
-export {};
