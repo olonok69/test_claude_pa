9e9a0e1bae9fee3ed0e4442eb56878ada79666fa
olonok69 <olonok@gmail.com>
2025-08-22 16:31:29 +0100

    prophet

--- renew-cert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo docker run --rm \
-    -v $(pwd)/certbot/conf:/etc/letsencrypt \
-    -v $(pwd)/certbot/www:/var/www/certbot \
-    certbot/certbot renew --quiet
-sudo docker compose restart nginx
